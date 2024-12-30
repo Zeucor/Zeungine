073fabd6c6bd20221d2b1c0e4a2604e2afb6d5e6
Steven French <mr.steven.french@gmail.com>
2024-12-30 13:40:02 +1300

    GLWindow test: add position and rotation to form model

--- tests/GLWindow.cpp
+++ tests/GLWindow.cpp
@@ -11,13 +11,18 @@ struct TestTriangle : anex::IEntity, vaos::VAO
   uint32_t indices[3];
   std::array<glm::vec4, 3> colors;
   std::array<glm::vec3, 3> positions;
+  glm::mat4 position;
+  glm::mat4 rotation;
+  float rotationAmount = 0;
   TestTriangle(anex::IWindow &window):
     IEntity(window),
-    VAO({"Color", "Position", "View", "Projection"}, 3),
+    VAO({"Color", "Position", "View", "Projection", "Model"}, 3),
     shader(constants),
     indices(0, 1, 2),
     colors({{1, 0, 0, 1}, {0, 1, 0, 1}, {0, 0, 1, 1}}),
-    positions({{0, 1, 0}, {1, -1, 0}, {-1, -1, 0}})
+    positions({{0, 1, 0}, {1, -1, 0}, {-1, -1, 0}}),
+    position(glm::translate(glm::mat4(1.0f), glm::vec3(0, 5, 0))),
+    rotation(glm::mat4(1.0f))
   {
     updateIndices(indices);
     updateElements("Color", colors.data());
@@ -27,6 +32,10 @@ struct TestTriangle : anex::IEntity, vaos::VAO
   };
   void render() override
   {
+    rotationAmount += glm::radians(0.01f);
+    rotation = glm::rotate(glm::mat4(1.0f), rotationAmount, glm::vec3(0, 0, 1));
+    auto model = position * rotation;
+    shader.setBlock("Model", model);
     shader.use(true);
     vaoDraw();
     shader.use(false);
