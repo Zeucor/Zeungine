417ae5f12bef9d6b88fdca03ee376518f2e81351
Steven French <mr.steven.french@gmail.com>
2025-01-30 16:51:11 +1300

    SimpleWindowTest: implement SimpleScene with a red clearColor

--- tests/SimpleWindowTest.cpp
+++ tests/SimpleWindowTest.cpp
@@ -1,7 +1,20 @@
 #include <zg/Window.hpp>
+#include <zg/Scene.hpp>
 using namespace zg;
+struct SimpleScene : Scene
+{
+	SimpleScene(Window &window):
+		Scene(window, {0, 10, 10}, {0, -1, -1}, 81.f)
+	{
+		clearColor = {1, 0, 0, 1};
+	};
+};
 int main()
 {
 	Window window("My Simple Window", 640, 480, -1, -1);
+	window.runOnThread([](auto &window)
+	{
+		window.setIScene(std::make_shared<SimpleScene>((Window &)window));
+	});
 	window.run();
 }
