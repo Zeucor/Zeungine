9fecbda9a87be0a24bd679d27f46d77af2503c8d
Steven French <mr.steven.french@gmail.com>
2025-02-02 14:22:53 +1300

    update GL/EGLRenderer.hpp

--- include/zg/renderers/EGLRenderer.hpp
+++ include/zg/renderers/EGLRenderer.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #ifdef USE_EGL
 #include "../common.hpp"
-#include "../interfaces/IRenderer.hpp"
 #include "../interfaces/IPlatformWindow.hpp"
+#include "../interfaces/IRenderer.hpp"
 namespace zg
 {
 	struct EGLRenderer : IRenderer
@@ -11,7 +11,7 @@ namespace zg
 		EGLSurface eglSurface = EGL_NO_SURFACE;
 		EGLContext eglContext = EGL_NO_CONTEXT;
 		EGLint major, minor;
-    	EGLConfig eglConfig;
+		EGLConfig eglConfig;
 		EGLRenderer();
 		~EGLRenderer() override;
 		void createContext(IPlatformWindow* platformWindowPointer) override;
@@ -20,35 +20,42 @@ namespace zg
 		void clearColor(glm::vec4 color) override;
 		void clear() override;
 		void viewport(glm::ivec4 vp) const override;
-		void setUniform(shaders::Shader &shader, const std::string_view name, const void *pointer, uint32_t size, enums::EUniformType uniformType) override;
-		void setBlock(shaders::Shader &shader, const std::string_view name, const void* pointer, size_t size) override;
+		void initShader(shaders::Shader& shader, const shaders::RuntimeConstants& constants,
+										const std::vector<shaders::ShaderType>& shaderTypes) override;
+		void setUniform(shaders::Shader& shader, const std::string_view name, const void* pointer, uint32_t size,
+										enums::EUniformType uniformType) override;
+		void setBlock(shaders::Shader& shader, const std::string_view name, const void* pointer, size_t size) override;
 		void deleteBuffer(uint32_t id) override;
-		void bindShader(const shaders::Shader &shader) override;
-		void unbindShader(const shaders::Shader &shader) override;
-		void addSSBO(shaders::Shader &shader, const std::string_view name, uint32_t bindingIndex) override;
-		void addUBO(shaders::Shader &shader, const std::string_view name, uint32_t bindingIndex) override;
-		void setSSBO(shaders::Shader &shader, const std::string_view name, const void *pointer, size_t size) override;
-		void setTexture(shaders::Shader &shader, const std::string_view name, const textures::Texture &texture, const int32_t unit) override;
-		bool compileShader(shaders::Shader &shader, shaders::ShaderType shaderType, shaders::ShaderPair &shaderPair) override;
-		bool compileProgram(shaders::Shader &shader, const shaders::ShaderMap& shaderMap) override;
-		bool checkCompileErrors(shaders::Shader &shader, const GLuint& id, bool isShader, const char* shaderType);
-		void deleteShader(shaders::Shader &shader) override;
-		void bindFramebuffer(const textures::Framebuffer &framebuffer) const override;
-		void unbindFramebuffer(const textures::Framebuffer &framebuffer) const override;
-		void initFramebuffer(textures::Framebuffer &framebuffer) override;
-		void destroyFramebuffer(textures::Framebuffer &framebuffer) override;
-		void bindTexture(const textures::Texture &texture) override;
-		void unbindTexture(const textures::Texture &texture) override;
-		void preInitTexture(textures::Texture &texture) override;
-		void midInitTexture(const textures::Texture &texture, const std::vector<images::ImageLoader::ImagePair>& images) override;
-		void postInitTexture(const textures::Texture &texture) override;
-		void destroyTexture(textures::Texture &texture) override;
-		void updateIndicesVAO(const vaos::VAO &vao, const std::vector<uint32_t> &indices) override;
-		void updateElementsVAO(const vaos::VAO &vao, const std::string_view constant, uint8_t *elementsAsChar) override;
-		void drawVAO(const vaos::VAO &vao) override;
-		void generateVAO(vaos::VAO &vao) override;
-		void destroyVAO(vaos::VAO &vao) override;
+		void bindShader(const shaders::Shader& shader) override;
+		void unbindShader(const shaders::Shader& shader) override;
+		void addSSBO(shaders::Shader& shader, const std::string_view name, uint32_t bindingIndex) override;
+		void addUBO(shaders::Shader& shader, const std::string_view name, uint32_t bindingIndex) override;
+		void setSSBO(shaders::Shader& shader, const std::string_view name, const void* pointer, size_t size) override;
+		void setTexture(shaders::Shader& shader, const std::string_view name, const textures::Texture& texture,
+										const int32_t unit) override;
+		bool compileShader(shaders::Shader& shader, shaders::ShaderType shaderType,
+											 shaders::ShaderPair& shaderPair) override;
+		bool compileProgram(shaders::Shader& shader, const shaders::ShaderMap& shaderMap) override;
+		bool checkCompileErrors(shaders::Shader& shader, const GLuint& id, bool isShader, const char* shaderType);
+		void deleteShader(shaders::Shader& shader) override;
+		void destroyShader(shaders::Shader& shader) override;
+		void bindFramebuffer(const textures::Framebuffer& framebuffer) const override;
+		void unbindFramebuffer(const textures::Framebuffer& framebuffer) const override;
+		void initFramebuffer(textures::Framebuffer& framebuffer) override;
+		void destroyFramebuffer(textures::Framebuffer& framebuffer) override;
+		void bindTexture(const textures::Texture& texture) override;
+		void unbindTexture(const textures::Texture& texture) override;
+		void preInitTexture(textures::Texture& texture) override;
+		void midInitTexture(const textures::Texture& texture,
+												const std::vector<images::ImageLoader::ImagePair>& images) override;
+		void postInitTexture(const textures::Texture& texture) override;
+		void destroyTexture(textures::Texture& texture) override;
+		void updateIndicesVAO(const vaos::VAO& vao, const std::vector<uint32_t>& indices) override;
+		void updateElementsVAO(const vaos::VAO& vao, const std::string_view constant, uint8_t* elementsAsChar) override;
+		void drawVAO(const vaos::VAO& vao) override;
+		void generateVAO(vaos::VAO& vao) override;
+		void destroyVAO(vaos::VAO& vao) override;
 	};
-	const bool GLcheck(const EGLRenderer &renderer, const char* fn, const bool egl = false);
-}
+	const bool GLcheck(const EGLRenderer& renderer, const char* fn, const bool egl = false);
+} // namespace zg
 #endif
