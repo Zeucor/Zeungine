40f27f10458bc59c89300646d74140d75b7d8789
Steven French <mr.steven.french@gmail.com>
2025-02-02 14:22:26 +1300

    update IRenderer with some new methods and format

--- include/zg/interfaces/IRenderer.hpp
+++ include/zg/interfaces/IRenderer.hpp
@@ -1,10 +1,11 @@
 #pragma once
+#include <map>
 #include <memory>
-#include <zg/glm.hpp>
 #include <zg/enums/EUniformType.hpp>
 #include <zg/enums/ShaderType.hpp>
+#include <zg/glm.hpp>
 #include <zg/images/ImageLoader.hpp>
-#include <map>
+#include <zg/shaders/RuntimeConstants.hpp>
 namespace zg
 {
 	struct IPlatformWindow;
@@ -13,12 +14,12 @@ namespace zg
 		struct Shader;
 		using ShaderPair = std::pair<std::string, uint32_t>;
 		using ShaderMap = std::map<ShaderType, ShaderPair>;
-	}
+	} // namespace shaders
 	namespace textures
 	{
 		struct Texture;
 		struct Framebuffer;
-	}
+	} // namespace textures
 	namespace vaos
 	{
 		struct VAO;
@@ -28,38 +29,45 @@ namespace zg
 		IPlatformWindow* platformWindowPointer = nullptr;
 		virtual ~IRenderer() = default;
 		virtual void init() = 0;
-		virtual void createContext(IPlatformWindow *platformWindowPointer) = 0;
+		virtual void createContext(IPlatformWindow* platformWindowPointer) = 0;
 		virtual void destroy() = 0;
 		virtual void clearColor(glm::vec4 color) = 0;
 		virtual void clear() = 0;
 		virtual void viewport(glm::ivec4 vp) const = 0;
-		virtual void setUniform(shaders::Shader &shader, const std::string_view name, const void *pointer, uint32_t size, enums::EUniformType uniformType) = 0;
-		virtual void setBlock(shaders::Shader &shader, const std::string_view name, const void* pointer, size_t size) = 0;
+		virtual void initShader(shaders::Shader& shader, const shaders::RuntimeConstants& constants,
+														const std::vector<shaders::ShaderType>& shaderTypes) = 0;
+		virtual void setUniform(shaders::Shader& shader, const std::string_view name, const void* pointer, uint32_t size,
+														enums::EUniformType uniformType) = 0;
+		virtual void setBlock(shaders::Shader& shader, const std::string_view name, const void* pointer, size_t size) = 0;
 		virtual void deleteBuffer(uint32_t id) = 0;
-		virtual void bindShader(const shaders::Shader &shader) = 0;
-		virtual void unbindShader(const shaders::Shader &shader) = 0;
-		virtual void addSSBO(shaders::Shader &shader, const std::string_view name, uint32_t bindingIndex) = 0;
-		virtual void addUBO(shaders::Shader &shader, const std::string_view name, uint32_t bindingIndex) = 0;
-		virtual void setSSBO(shaders::Shader &shader, const std::string_view name, const void *pointer, size_t size) = 0;
-		virtual void setTexture(shaders::Shader &shader, const std::string_view name, const textures::Texture &texture, const int32_t unit) = 0;
-		virtual bool compileShader(shaders::Shader &shader, shaders::ShaderType shaderType, shaders::ShaderPair &shaderPair) = 0;
-		virtual bool compileProgram(shaders::Shader &shader, const shaders::ShaderMap& shaderMap) = 0;
-		virtual void deleteShader(shaders::Shader &shader) = 0;
-		virtual void bindFramebuffer(const textures::Framebuffer &framebuffer) const = 0;
-		virtual void unbindFramebuffer(const textures::Framebuffer &framebuffer) const = 0;
-		virtual void initFramebuffer(textures::Framebuffer &framebuffer) = 0;
-		virtual void destroyFramebuffer(textures::Framebuffer &framebuffer) = 0;
-		virtual void bindTexture(const textures::Texture &texture) = 0;
-		virtual void unbindTexture(const textures::Texture &texture) = 0;
-		virtual void preInitTexture(textures::Texture &texture) = 0;
-		virtual void midInitTexture(const textures::Texture &texture, const std::vector<images::ImageLoader::ImagePair>& images) = 0;
-		virtual void postInitTexture(const textures::Texture &texture) = 0;
-		virtual void destroyTexture(textures::Texture &texture) = 0;
-		virtual void updateIndicesVAO(const vaos::VAO &vao, const std::vector<uint32_t> &indices) = 0;
-		virtual void updateElementsVAO(const vaos::VAO &vao, const std::string_view constant, uint8_t *elementsAsChar) = 0;
-		virtual void drawVAO(const vaos::VAO &vao) = 0;
-		virtual void generateVAO(vaos::VAO &vao) = 0;
-		virtual void destroyVAO(vaos::VAO &vao) = 0;
+		virtual void bindShader(const shaders::Shader& shader) = 0;
+		virtual void unbindShader(const shaders::Shader& shader) = 0;
+		virtual void addSSBO(shaders::Shader& shader, const std::string_view name, uint32_t bindingIndex) = 0;
+		virtual void addUBO(shaders::Shader& shader, const std::string_view name, uint32_t bindingIndex) = 0;
+		virtual void setSSBO(shaders::Shader& shader, const std::string_view name, const void* pointer, size_t size) = 0;
+		virtual void setTexture(shaders::Shader& shader, const std::string_view name, const textures::Texture& texture,
+														const int32_t unit) = 0;
+		virtual bool compileShader(shaders::Shader& shader, shaders::ShaderType shaderType,
+															 shaders::ShaderPair& shaderPair) = 0;
+		virtual bool compileProgram(shaders::Shader& shader, const shaders::ShaderMap& shaderMap) = 0;
+		virtual void deleteShader(shaders::Shader& shader) = 0;
+		virtual void destroyShader(shaders::Shader& shader) = 0;
+		virtual void bindFramebuffer(const textures::Framebuffer& framebuffer) const = 0;
+		virtual void unbindFramebuffer(const textures::Framebuffer& framebuffer) const = 0;
+		virtual void initFramebuffer(textures::Framebuffer& framebuffer) = 0;
+		virtual void destroyFramebuffer(textures::Framebuffer& framebuffer) = 0;
+		virtual void bindTexture(const textures::Texture& texture) = 0;
+		virtual void unbindTexture(const textures::Texture& texture) = 0;
+		virtual void preInitTexture(textures::Texture& texture) = 0;
+		virtual void midInitTexture(const textures::Texture& texture,
+																const std::vector<images::ImageLoader::ImagePair>& images) = 0;
+		virtual void postInitTexture(const textures::Texture& texture) = 0;
+		virtual void destroyTexture(textures::Texture& texture) = 0;
+		virtual void updateIndicesVAO(const vaos::VAO& vao, const std::vector<uint32_t>& indices) = 0;
+		virtual void updateElementsVAO(const vaos::VAO& vao, const std::string_view constant, uint8_t* elementsAsChar) = 0;
+		virtual void drawVAO(const vaos::VAO& vao) = 0;
+		virtual void generateVAO(vaos::VAO& vao) = 0;
+		virtual void destroyVAO(vaos::VAO& vao) = 0;
 	};
 	std::shared_ptr<IRenderer> createRenderer();
-}
+} // namespace zg
