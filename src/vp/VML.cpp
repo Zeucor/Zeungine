8432dcd639c6ac3f6615adb7d4bd64722d56f1fd
Steven French <mr.steven.french@gmail.com>
2025-02-09 08:14:25 +1300

    VML: fix window copy (now reference)

--- src/vp/VML.cpp
+++ src/vp/VML.cpp
@@ -1,36 +1,36 @@
-#include <zg/vp/VML.hpp>
 #include <zg/interfaces/IWindow.hpp>
+#include <zg/vp/VML.hpp>
 using namespace zg::vp;
-VML::VML(Scene &scene) : scene(scene)
+VML::VML(Scene& scene) : scene(scene)
 {
-  auto &window = scene.window;
-  mouseMoveID = window.addMouseMoveHandler(std::bind(&VML::mouseMoveHandler, this, std::placeholders::_1));
-  focusID = window.addFocusHandler(std::bind(&VML::focusHandler, this, std::placeholders::_1));
+	auto& window = scene.window;
+	mouseMoveID = window.addMouseMoveHandler(std::bind(&VML::mouseMoveHandler, this, std::placeholders::_1));
+	focusID = window.addFocusHandler(std::bind(&VML::focusHandler, this, std::placeholders::_1));
 };
 VML::~VML()
 {
-  scene.window.removeMouseMoveHandler(mouseMoveID);
-  scene.window.removeFocusHandler(focusID);
+	scene.window.removeMouseMoveHandler(mouseMoveID);
+	scene.window.removeFocusHandler(focusID);
 };
 void VML::mouseMoveHandler(glm::vec2 coords)
 {
-  if (scene.window.justWarpedPointer)
-  {
-    scene.window.justWarpedPointer = false;
-    return;
-  }
-  if (!scene.window.focused)
-    return;
-  glm::vec2 center = {scene.window.windowWidth / 2, scene.window.windowHeight / 2};
-  auto currentWindow = (Window &)scene.window;
-  auto diff = coords - center;
-  scene.view.addPhiTheta(diff.x * 0.001f, -diff.y * 0.001f);
-  scene.window.warpPointer(center);
+	if (scene.window.justWarpedPointer)
+	{
+		scene.window.justWarpedPointer = false;
+		return;
+	}
+	if (!scene.window.focused)
+		return;
+	glm::vec2 center = {scene.window.windowWidth / 2, scene.window.windowHeight / 2};
+	auto& currentWindow = (Window&)scene.window;
+	auto diff = coords - center;
+	scene.view.addPhiTheta(diff.x * 0.001f, -diff.y * 0.001f);
+	scene.window.warpPointer(center);
 }
 void VML::focusHandler(bool focused)
 {
-  if (focused)
-    scene.window.iPlatformWindow->hidePointer();
-  else
-    scene.window.iPlatformWindow->showPointer();
+	if (focused)
+		scene.window.iPlatformWindow->hidePointer();
+	else
+		scene.window.iPlatformWindow->showPointer();
 }
