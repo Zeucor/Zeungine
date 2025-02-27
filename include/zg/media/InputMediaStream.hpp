d774a508a3d55fa206dcefb3386e4301cf014578
Steven French <mr.steven.french@gmail.com>
2025-02-27 18:03:09 +1300

    some work on InputMediaStream

--- include/zg/media/InputMediaStream.hpp
+++ include/zg/media/InputMediaStream.hpp
@@ -1,32 +1,39 @@
 #pragma once
 #include <zg/Standard.hpp>
 #include "I1xCoder.hpp"
-extern "C"
-{
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
 #include <libavutil/frame.h>
-#include <libavutil/time.h>
 #include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libavutil/time.h>
+#include <libavutil/timestamp.h>
 #include <libswresample/swresample.h>
 #include <libswscale/swscale.h>
-#include <libavutil/opt.h>
-#include <libavutil/timestamp.h>
-#include <libavutil/avutil.h>
 }
 namespace zg::media
 {
-    struct InputMediaStream
-    {
-        std::string uri;
-        std::shared_ptr<interfaces::IFile> filePointer;
-        AVIOContext* ioContext = 0;
-        AVFormatContext* formatContext = 0;
-        std::vector<std::tuple<int32_t, AVStream*, std::shared_ptr<I1xCoder>, std::queue<AVFrame *>, std::mutex>> coderStreams;
-        bool playing = false;
-        InputMediaStream(const std::string &uri);
-        InputMediaStream(const std::string &uri, const std::shared_ptr<interfaces::IFile>& filePointer);
-        size_t open();
-        size_t close();
-    };
-}
+	struct InputMediaStream
+	{
+		using CoderStreamTuple =
+			std::tuple<int32_t, AVStream*, std::shared_ptr<I1xCoder>, std::queue<AVFrame*>, std::mutex>;
+		std::string uri;
+		std::shared_ptr<interfaces::IFile> filePointer;
+		unsigned char* streamBytes = 0;
+		AVIOContext* ioContext = 0;
+		AVFormatContext* formatContext = 0;
+		std::vector<CoderStreamTuple> coderStreams;
+		bool playing = false;
+		InputMediaStream(const std::string& uri);
+		InputMediaStream(const std::string& uri, const std::shared_ptr<interfaces::IFile>& filePointer);
+		size_t open();
+		size_t close();
+
+	private:
+		int32_t findStreamIndex(int i);
+		AVStream* findAVStream(int i, int32_t streamIndex);
+		std::shared_ptr<I1xCoder> construct1xCoder(int i, int32_t streamIndex, AVStream* stream);
+	};
+} // namespace zg::media
--- src/media/InputMediaStream.cpp
+++ src/media/InputMediaStream.cpp
@@ -13,9 +13,70 @@ InputMediaStream::InputMediaStream(const std::string &uri, const std::shared_ptr
 }
 size_t InputMediaStream::open()
 {
+    if (filePointer)
+    {
+        auto& file = *filePointer;
+        if (!file.isOpen())
+        {
+            return 0;
+        }
+        auto streamSize = file.size();
+        streamBytes = (uint8_t*)av_malloc(streamSize + 1);
+        file.readBytes(0, streamSize, streamBytes);
+        ioContext = avio_alloc_context(streamBytes, streamSize, 0, 0, 0, 0, 0);
+        streamBytes = 0;
+        if (!ioContext)
+        {
+            return 0;
+        }
+        formatContext = avformat_alloc_context();
+        formatContext->pb = ioContext;
+        formatContext->flags |= AVFMT_FLAG_CUSTOM_IO;
+        if (avformat_open_input(&formatContext, "", 0, 0) < 0)
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        formatContext = avformat_alloc_context();
+        if (avformat_open_input(&formatContext, uri.c_str(), 0, 0))
+        {
+            std::cerr << "Error opening '" << uri << ' formatContext' << std::endl;
+            return 0;
+        }
+    }
+    if (avformat_find_stream_info(formatContext, 0) < 0)
+    {
+        return 0;
+    }
+    for (auto i = 0; i <= 1; ++i)
+    {
+        auto StreamIndex = findStreamIndex(i);
+        auto avStream = findAVStream(i, StreamIndex);
+        auto i1xCoder = construct1xCoder(i, StreamIndex, avStream);
+        i1xCoder->open();
+        CoderStreamTuple tuple;
+        std::get<0>(tuple) = StreamIndex;
+        std::get<1>(tuple) = avStream;
+        std::get<2>(tuple) = i1xCoder;
+        coderStreams.push_back(tuple);
+    }
     return /*s*/ 1;
 }
 size_t InputMediaStream::close()
+{
+    return 0;
+}
+int32_t InputMediaStream::findStreamIndex(int i)
+{
+    return 0;
+}
+AVStream* InputMediaStream::findAVStream(int i, int32_t streamIndex)
+{
+    return 0;
+}
+std::shared_ptr<I1xCoder> InputMediaStream::construct1xCoder(int i, int32_t streamIndex, AVStream* stream)
 {
     return 0;
 }
