c8c42408760df45d06b0471738ad9776df88ebcc
Steven French <mr.steven.french@gmail.com>
2025-03-09 00:29:06 +1300

    some windows packaging fixes

--- auto-install.bat
+++ auto-install.bat
@@ -2,15 +2,15 @@
 setlocal enabledelayedexpansion
 :: Define version and targets
 set VERSION=0.2.3.5
-set TARGETS=ZeungineDependencies ZeungineDependencies-debug ZeungineHeaders Zeungine Zeungine-debug
+set TARGETS=ZeungineDependencies-debug ZeungineDependencies ZeungineHeaders Zeungine Zeungine-debug
 :: Iterate over targets and run them
 for %%T in (%TARGETS%) do (
     set EXECUTABLE=%%T-%VERSION%.exe
     if exist !EXECUTABLE! (
-        echo Running !EXECUTABLE! /S
+        echo Quietly installing !EXECUTABLE! /S
         !EXECUTABLE! /S
     ) else (
-        echo Warning: !EXECUTABLE! not found.
+        echo Installer !EXECUTABLE! not found.
     )
 )
 endlocal
