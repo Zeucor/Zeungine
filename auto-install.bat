df311dfdfb8cfe7f1639f551b5abd88c2753e6ac
Steven French <mr.steven.french@gmail.com>
2025-03-08 20:22:55 +1300

    add auto-install.bat, to be used with .zip windows releases

---
+++ auto-install.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+:: Define version and targets
+set VERSION=0.2.3
+set TARGETS=ZeungineDependencies ZeungineDependencies-debug ZeungineHeaders Zeungine Zeungine-debug
+:: Iterate over targets and run them
+for %%T in (%TARGETS%) do (
+    set EXECUTABLE=%%T-%VERSION%.exe
+    if exist !EXECUTABLE! (
+        echo Running !EXECUTABLE! /S
+        !EXECUTABLE! /S
+    ) else (
+        echo Warning: !EXECUTABLE! not found.
+    )
+)
+endlocal
