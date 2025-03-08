736ecce075a136d354d94d02a898af57cb961ba0
Steven French <mr.steven.french@gmail.com>
2025-03-08 15:23:32 +1300

    spicy echos in ./unx-package

--- unx-package.sh
+++ unx-package.sh
@@ -1,19 +1,60 @@
 #!/bin/bash
+
 cd cmake/Dependencies
+
+
+echo " -- Starting zegndeps Debug Configure"
 cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Debug -DZG_PACKAGE=ON
+
+echo " -- Starting zegndeps Debug Build"
 cmake --build build
+
+echo " -- Starting zegndeps Debug Package"
 cpack --config build/CPackConfig.cmake -C Debug
+
+
+echo " -- Starting zegndeps Release Configure"
 cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Release -DZG_PACKAGE=ON
+
+echo " -- Starting zegndeps Release Build"
 cmake --build build
+
+echo " -- Starting zegndeps Release Package"
 cpack --config build/CPackConfig.cmake -C Release
+
+
+
 cd ../Headers
-cmake -GNinja -B build-headers -DCMAKE_BUILD_TYPE=Release -DZG_PACKAGE=ON
-cpack --config build-headers/CPackConfig.cmake -C Release
-cd ../..
-cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Debug -DZG_PACKAGE=ON
-cmake --build build
-cpack --config build/CPackConfig.cmake -C Debug
+
+
+echo " -- Starting zeungine Headers Configure"
 cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Release -DZG_PACKAGE=ON
-cmake --build build
+
+echo " -- Starting zeungine Headers Package"
 cpack --config build/CPackConfig.cmake -C Release
+
+
+cd ../..
+
+
+echo " -- Starting zeungine Debug Configure"
+cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Debug -DZG_PACKAGE=ON
+
+echo " -- Starting zeungine Debug Build"
+cmake --build build
+
+echo " -- Starting zeungine Debug Package"
+cpack --config build/CPackConfig.cmake -C Debug
+
+
+echo " -- Starting zeungine Release Configure"
+cmake -GNinja -B build -DCMAKE_BUILD_TYPE=Release -DZG_PACKAGE=ON
+
+echo " -- Starting zeungine Release Build"
+cmake --build build
+
+echo " -- Starting zeungine Release Package"
+cpack --config build/CPackConfig.cmake -C Release
+
+
 ls -lah releases
