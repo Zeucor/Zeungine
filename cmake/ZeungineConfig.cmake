d478b3c28a5ed95cb6fb01433903757f36e50d4c
Steven French <mr.steven.french@gmail.com>
2025-03-09 17:11:28 +1300

    fix for zg_setup_target

--- cmake/ZeungineConfig.cmake
+++ cmake/ZeungineConfig.cmake
@@ -2,9 +2,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/PlatformSetup.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Options.cmake)
 include(FindPackageHandleStandardArgs)
 set(Zeungine_INCLUDE_DIR ${ZG_INC_INSTALL_PREFIX_ABS})
-zg_setup_target(zeungine ${ZG_LIBRARY_TYPE}
-    ${ZG_LIB_INSTALL_PREFIX_ABS}
-    ${ZG_LIB_PREFIX} zeungine zeungine ${SHARED_ZG_LIB_SUFFIX})
+zg_setup_target(zeungine "${ZG_LIBRARY_TYPE}"
+    "${ZG_LIB_INSTALL_PREFIX_ABS}"
+    "${ZG_LIB_PREFIX}" zeungine zeungine "${SHARED_ZG_LIB_SUFFIX}")
 set(Zeungine_LIBRARIES ${LAST_LIBRARY_LOCATION})
 find_package_handle_standard_args(Zeungine
 	REQUIRED_VARS Zeungine_INCLUDE_DIR Zeungine_LIBRARIES
