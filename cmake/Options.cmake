092c7cdeefe6528e8c9f68c6607bdceb80193046
Steven French <mr.steven.french@gmail.com>
2025-03-11 19:49:48 +1300

    always enable ZG_PACKAGE

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -6,7 +6,7 @@ option(BUILD_VULKAN         "Whether to build Vulkan module" ON)
 option(BUILD_EDITOR         "Whether to build the Zeungine editor" ON)
 option(ZG_INSTALL           "Whether to install Zeungine" ON)
 option(ZG_INSTALL_TESTS     "Whether to install Zeungine tests" ON)
-option(ZG_PACKAGE           "Whether to package Zeungine" OFF)
+option(ZG_PACKAGE           "Whether to package Zeungine" ON)
 set   (ZG_SRC_ABS           ${CMAKE_CURRENT_LIST_DIR}/..) # valid in, in source configures only
 get_filename_component(ZG_SRC_ABS "${ZG_SRC_ABS}" ABSOLUTE)
 if(ZG_INSTALL)
