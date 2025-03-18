5b55c5f73c4dd5614d90b87f4723584c2f4a320c
Steven French <mr.steven.french@gmail.com>
2025-03-18 16:46:04 +1300

    update tag.yml and change to CURVER_CHANGELOG.md

--- release-tag.sh
+++ release-tag.sh
@@ -15,15 +15,15 @@ PREV_HASH=$(git for-each-ref --sort=-creatordate --format '%(objectname)' refs/t
 PREV_TAG=$(git for-each-ref --sort=-creatordate --format '%(refname:strip=2)' refs/tags | sed -n 2p)
 CURR_HASH=$(git rev-parse HEAD)
 
-echo "## Changes since $PREV_TAG" > CURTAG_CHANGELOG.md
-echo "" >> CURTAG_CHANGELOG.md
-git log $PREV_HASH..$CURR_HASH --pretty=format:"- %h %s (%an, %ar)" >> CURTAG_CHANGELOG.md
-echo "" >> CURTAG_CHANGELOG.md
+echo "## Changes since $PREV_TAG" > CURVER_CHANGELOG.md
+echo "" >> CURVER_CHANGELOG.md
+git log $PREV_HASH..$CURR_HASH --pretty=format:"- %h %s (%an, %ar)" >> CURVER_CHANGELOG.md
+echo "" >> CURVER_CHANGELOG.md
 
-(cat CURTAG_CHANGELOG.md && cat CHANGELOG.md) > NEW_CHANGELOG.md
+(cat CURVER_CHANGELOG.md && cat CHANGELOG.md) > NEW_CHANGELOG.md
 mv NEW_CHANGELOG.md CHANGELOG.md
 
-git add CHANGELOG.md CURTAG_CHANGELOG.md
+git add CHANGELOG.md CURVER_CHANGELOG.md
 git commit -m "Update CHANGELOG for $TAG"
 
 git tag "$TAG"
