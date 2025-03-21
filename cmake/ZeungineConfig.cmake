a951df1ad80b520f0dbd103c3b6ab5350f2f8c8e
Steven French <mr.steven.french@gmail.com>
2025-03-21 22:29:50 +1300

    ZeungineConfig.cmake: add zeungine to Zeungine_LIBRARIES

--- cmake/ZeungineConfig.cmake
+++ cmake/ZeungineConfig.cmake
@@ -11,7 +11,7 @@ zg_setup_target(zeungine "${ZG_LIBRARY_TYPE}"
     "${ZG_LIB_INSTALL_PREFIX_ABS}"
     "${ZG_LIB_PREFIX}" zeungine zeungine "${zeungine_LIB_SUFFIX}" OFF)
 include(${CMAKE_CURRENT_LIST_DIR}/Targets.cmake)
-set(Zeungine_LIBRARIES "${ZG_LIBRARIES}")
+set(Zeungine_LIBRARIES zeungine ${ZG_LIBRARIES})
 find_package_handle_standard_args(Zeungine
 	REQUIRED_VARS Zeungine_INCLUDE_DIR Zeungine_LIBRARIES
 	VERSION_VAR Zeungine_VERSION)
