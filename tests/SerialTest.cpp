4510d2b11fa7ab87b0432d8724b9519d3451f9b1
Steven French <mr.steven.french@gmail.com>
2025-03-23 16:01:01 +1300

    SerialTest: write/read 1024 random bits

--- tests/SerialTest.cpp
+++ tests/SerialTest.cpp
@@ -8,14 +8,23 @@
 #endif
 #include <zg/net/streams/client_tcp_iostream.hpp>
 #include <zg/net/tcp_server.hpp>
+#include <zg/crypto/Random.hpp>
 void server();
 std::shared_ptr<zg::net::tcp_server> tcpServer;
 #define PORT 8080
 #define BITSTREAM true
+#define BITSLENGTH 1024
+std::vector<bool> clientBits(BITSLENGTH);
+std::vector<bool> serverBits(BITSLENGTH);
 void client();
 int main()
 {
 	tcpServer = std::make_shared<zg::net::tcp_server>(PORT, BITSTREAM);
+	auto serverBitsSize = serverBits.size();
+	for (auto i = 0; i < serverBitsSize; ++i)
+	{
+		serverBits[i] = zg::crypto::Random::value<short>(0, 1);
+	}
 	std::thread clientThread(client);
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	std::thread serverThread(server);
@@ -26,14 +35,7 @@ void server()
 {
 	auto& clientTuple = tcpServer->acceptOne();
 	auto& clientSerial = *std::get<1>(clientTuple);
-	bool bitCon[7] = {false};
-	for (auto e = 0; e < 7; e++)
-	{
-		clientSerial >> bitCon[e];
-		zg::Logger::print(zg::Logger::Blank, "SERVER: received bit: ", (bitCon[e] ? "true" : "false"));
-		clientSerial << bitCon[e];
-		zg::Logger::print(zg::Logger::Blank, "SERVER: sent bit: ", (bitCon[e] ? "true" : "false"));
-	}
+	clientSerial.writeBits(serverBits, 0, serverBits.size());
 	clientSerial.synchronize();
 	zg::Logger::print(zg::Logger::Blank, "SERVER: synchronized bits");
 }
@@ -59,11 +61,11 @@ void client()
 	}
 	zg::net::streams::client_tcp_iostream tcp_in_out(sock);
 	Serial serial(tcp_in_out, BITSTREAM);
-	zg::Logger::print(zg::Logger::Blank, "CLIENT: Sending Bits");
-	(serial << true << true << true << false << true << true << true).synchronize();
-	zg::Logger::print(zg::Logger::Blank, "CLIENT: Sent Bits");
-	bool bitCon[7] = {false};
-	for (auto e = 0; e < 7; e++)
-		serial >> bitCon[e];
+	serial.readBits(clientBits, 0, clientBits.size());
+	for (const auto &bit : clientBits)
+	{
+		zg::Logger::print(zg::Logger::Blank, "Client Bit: ", bit ? "1" : "0");
+	}
+	assert(clientBits == serverBits);
 	zg::Logger::print(zg::Logger::Blank, "CLIENT: Received Bits");
 }
