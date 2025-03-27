1593fdde4b81e5c5cfa9041acdb0892728a9bee2
Steven French <mr.steven.french@gmail.com>
2025-03-27 18:09:47 +1300

    use openssl 3.4.1

--- cmake/Dependencies/Build.cmake
+++ cmake/Dependencies/Build.cmake
@@ -17,7 +17,7 @@ endif()
 # OpenSSL
 FetchContent_Declare(openssl
     GIT_REPOSITORY https://github.com/openssl/openssl.git
-    GIT_TAG openssl-3.0.9)
+    GIT_TAG openssl-3.4.1)
 FetchContent_GetProperties(openssl)
 if(NOT openssl_POPULATED)
     FetchContent_Populate(openssl)
