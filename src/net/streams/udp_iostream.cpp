c74aadb6f5e1f5324562a9e73fec377f5fcfa140
Steven French <mr.steven.french@gmail.com>
2025-03-28 14:45:12 +1300

    udp_iostream: format document

--- src/net/streams/udp_iostream.cpp
+++ src/net/streams/udp_iostream.cpp
@@ -1,16 +1,7 @@
 #include <zg/net/streams/udp_iostream.hpp>
 using namespace zg::net::streams;
-udp_istream::udp_istream(const udp_streambuf::SocketPair& fd_addr_pair) :
-		std::istream(&buf), buf(fd_addr_pair)
-{
-}
-udp_ostream::udp_ostream(const udp_streambuf::SocketPair& fd_addr_pair) :
-		std::ostream(&buf), buf(fd_addr_pair)
-{
-}
+udp_istream::udp_istream(const udp_streambuf::SocketPair& fd_addr_pair) : std::istream(&buf), buf(fd_addr_pair) {}
+udp_ostream::udp_ostream(const udp_streambuf::SocketPair& fd_addr_pair) : std::ostream(&buf), buf(fd_addr_pair) {}
 void udp_ostream::pushData(const char* data, size_t length) { buf.setReceivedData(data, length); }
-udp_iostream::udp_iostream(const udp_streambuf::SocketPair& fd_addr_pair) :
-		std::iostream(&buf), buf(fd_addr_pair)
-{
-}
+udp_iostream::udp_iostream(const udp_streambuf::SocketPair& fd_addr_pair) : std::iostream(&buf), buf(fd_addr_pair) {}
 void udp_iostream::pushData(const char* data, size_t length) { buf.setReceivedData(data, length); }
