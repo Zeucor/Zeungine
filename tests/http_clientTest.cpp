f983f5fb2de4209e8e10bb36a503b41f78e288a1
Steven French <mr.steven.french@gmail.com>
2025-04-01 11:03:23 +1300

    change http_clientTest url to something shorter

--- tests/http_clientTest.cpp
+++ tests/http_clientTest.cpp
@@ -2,7 +2,7 @@
 #include <zg/Logger.hpp>
 int main()
 {
-    auto response = zg::net::http::http_client::restSync("GET", "https://search.brave.com/");
+    auto response = zg::net::http::http_client::restSync("GET", "https://phet-dev.colorado.edu/html/build-an-atom/0.0.0-3/simple-text-only-test-page.html");
     zg::Logger::print(
         zg::Logger::Blank,
         "StatusCode: ",
