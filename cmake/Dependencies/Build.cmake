dfba326d20a9c1f5d9f46405989bc0fe838da5ba
Steven French <mr.steven.french@gmail.com>
2025-04-07 13:10:00 +1200

    remove boost filesystem

--- cmake/Dependencies/Build.cmake
+++ cmake/Dependencies/Build.cmake
@@ -203,8 +203,10 @@ file(GLOB BOOST_LIB_SOURCES
     "${boost_SOURCE_DIR}/libs/*/src/*.c"
 )
 
-# Building Boost without support for: atomic, python, graph_parallel, stacktrace, mpi, container (dlmalloc)
+# Building Boost without support for: filesystem, atomic, python, graph_parallel, stacktrace, mpi, container (dlmalloc)
 
+list(FILTER BOOST_LIB_SOURCES EXCLUDE REGEX "^${boost_SOURCE_DIR}/libs/filesystem/src/.*\\.cpp$")
+list(FILTER BOOST_LIB_SOURCES EXCLUDE REGEX "^${boost_SOURCE_DIR}/libs/filesystem/src/.*\\.c$")
 list(FILTER BOOST_LIB_SOURCES EXCLUDE REGEX "^${boost_SOURCE_DIR}/libs/atomic/src/.*\\.cpp$")
 list(FILTER BOOST_LIB_SOURCES EXCLUDE REGEX "^${boost_SOURCE_DIR}/libs/atomic/src/.*\\.c$")
 list(FILTER BOOST_LIB_SOURCES EXCLUDE REGEX "^${boost_SOURCE_DIR}/libs/python/src/.*\\.cpp$")
