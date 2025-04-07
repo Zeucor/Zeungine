52513e0e4e8d16175ef7d8c43ec42ec5445dd372
Steven French <mr.steven.french@gmail.com>
2025-04-07 19:42:35 +1200

    some fixes to dependencies install for macos

--- cmake/Dependencies/Install.cmake
+++ cmake/Dependencies/Install.cmake
@@ -43,50 +43,14 @@ set(ZG_TARGETS_TO_INSTALL
 set(TARGET_ARTIFACT_FILES_TO_INSTALL "")
 foreach(TGT ${ZG_TARGETS_TO_INSTALL})
     if(NOT TARGET ${TGT})
-        message(WARNING "Target ${TGT} listed for install does not exist at configure time.")
         continue()
     endif()
-
-    get_target_property(TGT_TYPE ${TGT} TYPE)
-
-    get_target_property(LOC ${TGT} LOCATION)
-    if(LOC)
-       if(TGT_TYPE STREQUAL "STATIC_LIBRARY" OR TGT_TYPE STREQUAL "SHARED_LIBRARY") # Shared libs have import libs sometimes via LOCATION
-           if(EXISTS "${LOC}")
-              list(APPEND TARGET_ARTIFACT_FILES_TO_INSTALL "${LOC}")
-              message(STATUS "Configure-time path (using LOCATION) for ${TGT} ARCHIVE/IMPORT: ${LOC}")
-           else()
-              message(WARNING "Configure-time path (using LOCATION) for ${TGT} points to non-existent file: ${LOC}")
-           endif()
-       endif()
-    else()
-        get_target_property(ARCHIVE_DIR ${TGT} ARCHIVE_OUTPUT_DIRECTORY)
-        get_target_property(ARCHIVE_NAME_PROP ${TGT} ARCHIVE_OUTPUT_NAME)
-        if(NOT ARCHIVE_DIR)
-             set(ARCHIVE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
-        endif()
-        if(ARCHIVE_NAME_PROP AND TGT_TYPE STREQUAL "STATIC_LIBRARY")
-             get_target_property(PREFIX ${TGT} PREFIX)
-             get_target_property(SUFFIX ${TGT} SUFFIX) # e.g. ".lib" or ".a"
-             set(ARCHIVE_FILE_PATH "${ARCHIVE_DIR}/${PREFIX}${ARCHIVE_NAME_PROP}${SUFFIX}")
-             message(WARNING "Trying to predict archive path for ${TGT} at configure time is fragile: ${ARCHIVE_FILE_PATH}")
-        endif()
-    endif()
-    if(TGT_TYPE STREQUAL "SHARED_LIBRARY" OR TGT_TYPE STREQUAL "MODULE_LIBRARY" OR TGT_TYPE STREQUAL "EXECUTABLE")
-        get_target_property(SUFFIX ${TGT} SUFFIX)
-        if(WIN32 AND SUFFIX STREQUAL ".lib")
-            message(STATUS "Configure-time path (using LOCATION) for ${TGT} appears to be import lib: ${LOC}")
-        else()
-            list(APPEND TARGET_ARTIFACT_FILES_TO_INSTALL "${LOC}")
-            message(STATUS "Configure-time path (using LOCATION) for ${TGT} RUNTIME/LIBRARY: ${LOC}")
-        endif()
-        else()
-            message(WARNING "Cannot reliably determine configure-time RUNTIME/LIBRARY path for ${TGT}.")
-        endif()
-    endif()
+    set(TARGET_ARTIFACT_FILES_TO_INSTALL ${TARGET_ARTIFACT_FILES_TO_INSTALL} "${CMAKE_BINARY_DIR}/${ZG_LIB_PREFIX}${TGT}${TYPE_ZG_LIB_SUFFIX}")
 endforeach()
 
 set(ZG_LIBRARIES_TO_INSTALL ${ZG_LIBRARIES_TO_INSTALL} ${TARGET_ARTIFACT_FILES_TO_INSTALL})
+
+message(STATUS "ZG_LIBRARIES_TO_INSTALL: ${ZG_LIBRARIES_TO_INSTALL}")
 
 install(FILES ${ZG_LIBRARIES_TO_INSTALL}
     DESTINATION ${ZG_LIB_INSTALL_PREFIX}
