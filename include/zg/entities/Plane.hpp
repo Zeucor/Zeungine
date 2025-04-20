647aa521b35eafb3037ff7161ab916997c487466
Steven French <mr.steven.french@gmail.com>
2025-04-20 14:05:25 +1200

    Plane: implement PlaneFactory for colored and textured planes (textured not linked yet)

--- include/zg/entities/Plane.hpp
+++ include/zg/entities/Plane.hpp
@@ -1,34 +1,11 @@
 #pragma once
 #include <array>
 #include <zg/Entity.hpp>
-#include <zg/Scene.hpp>
 #include <zg/glm.hpp>
-
-namespace zg
-{
-	struct Window;
-}
 namespace zg::entities
 {
-	EntityCreateInfo PlaneFactory(glm::vec3 position, glm::quat rotation, glm::vec3 scale, glm::vec2 size,
-		glm::vec4 color, const shaders::RuntimeConstants& constants = {}, std::string_view name = "");
-	EntityCreateInfo PlaneFactory(glm::vec3 position, glm::quat rotation, glm::vec3 scale, glm::vec2 size,
-		textures::Texture& texture, const shaders::RuntimeConstants& constants = {}, std::string_view name = "");
-	// struct Plane : Entity
-	// {
-	// 	size_t getTypeID() override { return EntityTypeID<Plane>::id; }
-	// 	glm::vec4 color;
-	// 	std::vector<glm::vec4> colors;
-	// 	std::vector<glm::vec2> uvs;
-	// 	std::vector<glm::vec3> normals = {};
-	// 	textures::Texture* texturePointer = 0;
-	// 	glm::vec2 size;
-	// 	inline static size_t planesCount = 0;
-	// 	Plane(Window& window, );
-	// 	Plane(Window& window, );
-	// 	std::vector<uint32_t> getIndices(zg::Window& window);
-	// 	bool preRender() override;
-	// 	void setColor(glm::vec4 color);
-	// 	void setSize(glm::vec2 size);
-	// };
-} // namespace zg::entities
+	EntityCreateInfo PlaneFactory(glm::vec4 color = {1, 1, 1, 1}, std::string name = "", glm::vec3 position = {0, 0, 0}, glm::quat rotation = {1, 0, 0, 0}, glm::vec3 scale = {1, 1, 1}, glm::vec2 size = {2, 2},
+		const shaders::RuntimeConstants& constants = {}, zg::FRONTFACE frontFace = zg::IRenderer::DEFAULTFRONTFACE);
+	EntityCreateInfo PlaneFactory(textures::Texture& texture, std::string name = "", glm::vec3 position = {0, 0, 0}, glm::quat rotation = {1, 0, 0, 0}, glm::vec3 scale = {1, 1, 1}, glm::vec2 size = {2, 2},
+		const shaders::RuntimeConstants& constants = {}, zg::FRONTFACE frontFace = zg::IRenderer::DEFAULTFRONTFACE);
+}
--- src/entities/Plane.cpp
+++ src/entities/Plane.cpp
@@ -1,155 +1,75 @@
 #include <zg/entities/Plane.hpp>
 #include <zg/utilities.hpp>
-using namespace zg::entities;
-// Plane::Plane(zg::Window& window, zg::Scene& scene, glm::vec3 position, glm::quat rotation, glm::vec3 scale,
-// 						 glm::vec2 size, glm::vec4 color, const zg::shaders::RuntimeConstants& constants, std::string_view name) :
-// 		zg::Entity(window, scene,
-// 							 zg::mergeVectors<std::string>(
-// 								 {{"Color", "Position", "Normal", "View", "Projection", "Model", "CameraPosition"}}, constants),
-// 							 6, getIndices(window), 4,
-// 							 {
-// 								 {-size.x / 2, -size.y / 2, 0},
-// 								 {size.x / 2, -size.y / 2, 0},
-// 								 {size.x / 2, size.y / 2, 0},
-// 								 {-size.x / 2, size.y / 2, 0} // Front
-// 							 },
-// 							 position, rotation, scale, name.empty() ? "Plane " + std::to_string(++planesCount) : name),
-// 		uvs({{}, {}, {}, {}}), size(size)
-// {
-// 	computeNormals(window.iRenderer->frontFace, indices, vertices, normals);
-// 	updateIndices(indices);
-// 	setColor(color);
-// 	updateElements("Position", vertices);
-// 	updateElements("Normal", normals);
-// };
-// Plane::Plane(zg::Window& window, zg::Scene& scene, glm::vec3 position, glm::quat rotation, glm::vec3 scale,
-// 						 glm::vec2 size, textures::Texture& texture, const zg::shaders::RuntimeConstants& constants,
-// 						 std::string_view name) :
-// 		zg::Entity(
-// 			window, scene,
-// 			zg::mergeVectors<std::string>(
-// 				{{"UV2", "Position", "Normal", "Texture2D", "View", "Projection", "Model", "CameraPosition"}}, constants),
-// 			6, getIndices(window), 4,
-// 			{
-// 				{-size.x / 2, -size.y / 2, 0},
-// 				{size.x / 2, -size.y / 2, 0},
-// 				{size.x / 2, size.y / 2, 0},
-// 				{-size.x / 2, size.y / 2, 0} // Front
-// 			},
-// 			position, rotation, scale, name.empty() ? "Plane " + std::to_string(++planesCount) : name),
-// 		colors({{}, {}, {}, {}}), uvs({
-// 																// Front face
-// 																{0, 0}, // 0
-// 																{1, 0}, // 1
-// 																{1, 1}, // 2
-// 																{0, 1} // 3
-// 															}),
-// 		texturePointer(&texture), size(size)
-// {
-// 	switch (window.iRenderer->renderer)
-// 	{
-// 	default:
-// 		break;
-// 	case RENDERER_VULKAN:
-// 	case RENDERER_METAL:
-// 		flipUVsY(uvs);
-// 		break;
-// 	}
-// 	computeNormals(window.iRenderer->frontFace, indices, vertices, normals);
-// 	updateIndices(indices);
-// 	updateElements("UV2", uvs);
-// 	updateElements("Position", vertices);
-// 	updateElements("Normal", normals);
-// };
-// std::vector<uint32_t> Plane::getIndices(zg::Window& window)
-// {
-// 	if (window.iRenderer->frontFace == zg::CLOCKWISE)
-// 		return {{2, 1, 0, 0, 3, 2}};
-// 	else
-// 		return {{0, 1, 2, 2, 3, 0}};
-// }
-// bool Plane::preRender()
-// {
-// 	const auto& model = getModelMatrix();
-// 	auto shader = addShader();
-// 	shader->bind(*this);
-// 	scene.entityPreRender(*this);
-// 	shader->setBlock("Model", *this, model);
-// 	shader->setBlock("View", *this, viewPointer ? viewPointer->matrix : scene.viewPointer->matrix);
-// 	shader->setBlock("Projection", *this,
-// 									 projectionPointer ? projectionPointer->matrix : scene.projectionPointer->matrix);
-// 	shader->setBlock("CameraPosition", *this, scene.viewPointer->position, 16);
-// 	if (texturePointer)
-// 		shader->setTexture("Texture2D", *this, *texturePointer, 0);
-// 	shader->unbind();
-// 	return true;
-// };
-// void Plane::setColor(glm::vec4 color)
-// {
-// 	colors = {color, color, color, color};
-// 	updateElements("Color", colors);
-// };
-// void Plane::setSize(glm::vec2 size)
-// {
-// 	vertices = {
-// 		{-size.x / 2, -size.y / 2, 0},
-// 		{size.x / 2, -size.y / 2, 0},
-// 		{size.x / 2, size.y / 2, 0},
-// 		{-size.x / 2, size.y / 2, 0} // Front
-// 	};
-// 	updateElements("Position", vertices);
-// 	this->size = size;
-// };
-
-// template <>
-// Serial& serialize(Serial& serial, const std::shared_ptr<zg::entities::Plane>& planePointer)
-// {
-// 	if (!planePointer)
-// 	{
-// 		serial << false;
-// 		return serial;
-// 	}
-// 	auto& plane = *planePointer;
-// 	serial << true;
-// 	auto& position = plane.position;
-// 	auto& rotation = plane.rotation;
-// 	auto& scale = plane.scale;
-// 	auto& size = plane.size;
-// 	auto& color = plane.color;
-// 	auto& constants = plane.constants;
-// 	auto& name = plane.name;
-// 	serial << position << rotation << scale << size;
-// 	auto constantsSize = constants.size();
-// 	serial << constantsSize;
-// 	for (auto j = 0; j < constantsSize; j++)
-// 		serial << constants[j];
-// 	serial << name;
-// 	return serial;
-// }
-// template <>
-// Serial& deserialize(Serial& serial, std::shared_ptr<zg::entities::Plane>& planePointer)
-// {
-// 	bool wroteBit = false;
-// 	serial >> wroteBit;
-// 	if (!wroteBit)
-// 		return serial;
-// 	zg::Window* windowPointer = (zg::Window*)serial.getContextPointer("Window");
-// 	zg::Scene* scenePointer = (zg::Scene*)serial.getContextPointer("Scene");
-// 	glm::vec3 position(0);
-// 	glm::quat rotation(0, 0, 0, 0);
-// 	glm::vec3 scale(0);
-// 	glm::vec2 size(0);
-// 	glm::vec4 color(0);
-// 	zg::shaders::RuntimeConstants constants{};
-// 	std::string name{};
-// 	serial >> position >> rotation >> scale >> size >> color;
-// 	auto constantsSize = constants.size();
-// 	serial >> constantsSize;
-// 	constants.resize(constantsSize);
-// 	for (auto j = 0; j < constantsSize; j++)
-// 		serial >> constants[j];
-// 	serial >> name;
-// 	planePointer = std::make_shared<zg::entities::Plane>(*windowPointer, *scenePointer, position, rotation, scale, size,
-// 																											 color, constants, name);
-// 	return serial;
-// }
+using namespace zg;
+zg::EntityCreateInfo zg::entities::PlaneFactory(glm::vec4 color, std::string name, glm::vec3 position, glm::quat rotation, glm::vec3 scale, glm::vec2 size,
+    const shaders::RuntimeConstants& constants, zg::FRONTFACE frontFace)
+{
+	std::vector<glm::vec4> colors(4, color);
+	std::vector<glm::vec3> vertices({{
+		{-size.x / 2, -size.y / 2, 0},	 {size.x / 2, -size.y / 2, 0},
+		{size.x / 2, size.y / 2, 0},		 {-size.x / 2, size.y / 2, 0} // Front
+	}});
+	std::vector<uint32_t> indices;
+	if (frontFace == zg::CLOCKWISE)
+		indices = {
+			2,	1,	0,	0,	3,	2, // Front face
+		};
+	else
+		indices = {
+			0,	1,	2,	2,	3,	0, // Front face
+		};
+    zg::EntityCreateInfo info{
+        .typeName = "Plane",
+        .position = position,
+        .rotation = rotation,
+        .scale = scale,
+        .constants = zg::mergeVectors<std::string>(
+			{{"Color", "Position", "Normal", "View", "Projection", "Model", "CameraPosition"}}, constants),
+        .name = name,
+        .indiceCount = 6,
+        .indices = indices,
+        .vertexCount = 4,
+        .vertices = vertices,
+        .colors = colors
+    };
+    return info;
+}
+zg::EntityCreateInfo zg::entities::PlaneFactory(textures::Texture& texture, std::string name, glm::vec3 position, glm::quat rotation, glm::vec3 scale, glm::vec2 size,
+    const shaders::RuntimeConstants& constants, zg::FRONTFACE frontFace)
+{
+	std::vector<glm::vec2> uv2s({
+        {0, 0},
+        {1, 0},
+        {1, 1},
+        {0, 1}
+    });
+	std::vector<glm::vec3> vertices({{
+		{-size.x / 2, -size.y / 2, 0},	 {size.x / 2, -size.y / 2, 0},
+		{size.x / 2, size.y / 2, 0},		 {-size.x / 2, size.y / 2, 0} // Front
+	}});
+	std::vector<uint32_t> indices;
+	if (frontFace == zg::CLOCKWISE)
+		indices = {
+			2,	1,	0,	0,	3,	2, // Front face
+		};
+	else
+		indices = {
+			0,	1,	2,	2,	3,	0, // Front face
+		};
+    zg::EntityCreateInfo info{
+        .typeName = "Plane",
+        .position = position,
+        .rotation = rotation,
+        .scale = scale,
+        .constants = zg::mergeVectors<std::string>(
+			{{"UV2", "Position", "Normal", "Texture2D", "View", "Projection", "Model", "CameraPosition"}}, constants),
+        .name = name,
+        .indiceCount = 6,
+        .indices = indices,
+        .vertexCount = 4,
+        .vertices = vertices,
+        .uv2s = uv2s
+    };
+    return info;
+    // 		texturePointer(&texture), size(size)
+}
