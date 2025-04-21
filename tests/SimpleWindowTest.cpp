12c97398ed5d6e29bc77d0f3e6679ee3065af48a
Steven French <mr.steven.french@gmail.com>
2025-04-21 12:14:51 +1200

    SimpleWindowTest: print size of some core structs in bytes

--- tests/SimpleWindowTest.cpp
+++ tests/SimpleWindowTest.cpp
@@ -1,9 +1,13 @@
 #include <zg/Scene.hpp>
 #include <zg/Window.hpp>
 #include <zg/Registry.hpp>
+#include <iostream>
 using namespace zg;
 int main()
 {
+	std::cout << "sizeof(Window) = " << sizeof(Window) << "bytes" << std::endl <<
+				"sizeof(Scene) = " << sizeof(Scene) << "bytes" << std::endl <<
+				"sizeof(Entity)" << sizeof(Entity) << "bytes" << std::endl;
 	WindowCreateInfo windowCreateInfo{.title = "Simple Window", .windowWidth = 640, .windowHeight = 480};
 	auto window_tuple = zg::Registry::addWindow(windowCreateInfo);
 	auto& window = *std::get<KEY_ID_VECTOR_VALUE_INDEX>(window_tuple);
