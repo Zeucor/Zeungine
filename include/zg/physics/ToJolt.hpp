5d43d0f4022e5b74f686b4d768144e9fa331c8ac
Steven French <mr.steven.french@gmail.com>
2025-04-30 11:51:32 +1200

    ToJolt: add RMat44 to glm::mat4

--- include/zg/physics/ToJolt.hpp
+++ include/zg/physics/ToJolt.hpp
@@ -17,6 +17,28 @@ R ToJolt(T val)
     if constexpr (std::is_same_v<T, JPH::Quat>)
         if constexpr (std::is_same_v<R, glm::quat>)
             return glm::quat(val.GetW(), val.GetX(), val.GetY(), val.GetZ());
+    if constexpr (std::is_same_v<T, JPH::RMat44>)
+        if constexpr (std::is_same_v<R, glm::mat4>)
+        {
+            glm::mat4 ret;
+            ret[0][0] = val(0, 0);
+            ret[0][1] = val(1, 0);
+            ret[0][2] = val(2, 0);
+            ret[0][3] = val(3, 0);
+            ret[1][0] = val(0, 1);
+            ret[1][1] = val(1, 1);
+            ret[1][2] = val(2, 1);
+            ret[1][3] = val(3, 1);
+            ret[2][0] = val(0, 2);
+            ret[2][1] = val(1, 2);
+            ret[2][2] = val(2, 2);
+            ret[2][3] = val(3, 2);
+            ret[3][0] = val(0, 3);
+            ret[3][1] = val(1, 3);
+            ret[3][2] = val(2, 3);
+            ret[3][3] = val(3, 3);
+            return ret;
+        }
     R r;
     return r;
 }
