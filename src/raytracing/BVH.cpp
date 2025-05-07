bc4b77c96ff54d6a11e6cb56cb41dcaf749c634f
Steven French <mr.steven.french@gmail.com>
2025-05-07 16:55:57 +1200

    BVH: add/updateEntity, recurse children

--- src/raytracing/BVH.cpp
+++ src/raytracing/BVH.cpp
@@ -145,77 +145,91 @@ size_t BVH::addTriangle(const Tri& tri)
 }
 void BVH::addEntity(Entity& entity)
 {
-	for (auto& meshID : entity.meshIDs)
+	if (entity.addToBVH)
 	{
-		auto& mesh = Registry::getMesh(meshID);
-		auto indiceCount = mesh.indiceCount ? mesh.indiceCount(entity) : 0;
-		if (!indiceCount)
-			return;
-		auto indices = mesh.indices(entity);
-		auto vertices = mesh.vertices(entity);
-		auto indicesData = indices.data();
-		auto verticesData = vertices.data();
-		auto& model = entity.getModelMatrix();
-		for (size_t i = 0, c = 0; i < indiceCount; c++, i += 3)
+		for (auto& meshID : entity.meshIDs)
 		{
-			auto i0 = indicesData[i + 0];
-			auto i1 = indicesData[i + 1];
-			auto i2 = indicesData[i + 2];
-			auto v0 = verticesData[i0];
-			auto v1 = verticesData[i1];
-			auto v2 = verticesData[i2];
-			v0 = glm::vec3(model * glm::vec4(v0, 1.0f));
-			v1 = glm::vec3(model * glm::vec4(v1, 1.0f));
-			v2 = glm::vec3(model * glm::vec4(v2, 1.0f));
-			addTriangle({{v0.x, v0.y, v0.z}, {v1.x, v1.y, v1.z}, {v2.x, v2.y, v2.z}, {entity.ID, mesh.ID}});
+			auto& mesh = Registry::getMesh(meshID);
+			auto indiceCount = mesh.indiceCount ? mesh.indiceCount(entity) : 0;
+			if (!indiceCount)
+				return;
+			auto indices = mesh.indices(entity);
+			auto vertices = mesh.vertices(entity);
+			auto indicesData = indices.data();
+			auto verticesData = vertices.data();
+			auto& model = entity.getModelMatrix();
+			for (size_t i = 0, c = 0; i < indiceCount; c++, i += 3)
+			{
+				auto i0 = indicesData[i + 0];
+				auto i1 = indicesData[i + 1];
+				auto i2 = indicesData[i + 2];
+				auto v0 = verticesData[i0];
+				auto v1 = verticesData[i1];
+				auto v2 = verticesData[i2];
+				v0 = glm::vec3(model * glm::vec4(v0, 1.0f));
+				v1 = glm::vec3(model * glm::vec4(v1, 1.0f));
+				v2 = glm::vec3(model * glm::vec4(v2, 1.0f));
+				addTriangle({{v0.x, v0.y, v0.z}, {v1.x, v1.y, v1.z}, {v2.x, v2.y, v2.z}, {entity.ID, mesh.ID}});
+			}
 		}
+	}
+	for (auto& child : entity.children)
+	{
+		addEntity(child);
 	}
 }
 void BVH::updateEntity(Entity& entity)
 {
-	for (auto& meshID : entity.meshIDs)
+	if (entity.addToBVH)
 	{
-		auto& mesh = Registry::getMesh(meshID);
-		std::vector<size_t> indices;
-		size_t indicesCount = 0;
-		auto trianglesSize = triangles.size();
-		for (size_t i = 0; i < trianglesSize; ++i)
+		for (auto& meshID : entity.meshIDs)
 		{
-			if (triangles[i].userData.first == entity.ID && triangles[i].userData.second == mesh.ID)
+			auto& mesh = Registry::getMesh(meshID);
+			std::vector<size_t> indices;
+			size_t indicesCount = 0;
+			auto trianglesSize = triangles.size();
+			for (size_t i = 0; i < trianglesSize; ++i)
 			{
-				++indicesCount;
+				if (triangles[i].userData.first == entity.ID && triangles[i].userData.second == mesh.ID)
+				{
+					++indicesCount;
+				}
+			}
+			indices.reserve(indicesCount);
+			for (size_t i = 0; i < trianglesSize; ++i)
+			{
+				if (triangles[i].userData.first == entity.ID && triangles[i].userData.second == mesh.ID)
+				{
+					indices.push_back(i);
+				}
+			}
+			auto indiceCount = mesh.indiceCount ? mesh.indiceCount(entity) : 0;
+			if (!indiceCount)
+				return;
+			auto meshIndices = mesh.indices(entity);
+			auto meshVertices = mesh.vertices(entity);
+			auto indicesData = meshIndices.data();
+			auto verticesData = meshVertices.data();
+			auto& model = entity.getModelMatrix();
+			for (size_t i = 0, c = 0; i < indiceCount; c++, i += 3)
+			{
+				auto& triangleID = indices[c];
+				auto i0 = indicesData[i + 0];
+				auto i1 = indicesData[i + 1];
+				auto i2 = indicesData[i + 2];
+				auto v0 = verticesData[i0];
+				auto v1 = verticesData[i1];
+				auto v2 = verticesData[i2];
+				v0 = glm::vec3(model * glm::vec4(v0, 1.0f));
+				v1 = glm::vec3(model * glm::vec4(v1, 1.0f));
+				v2 = glm::vec3(model * glm::vec4(v2, 1.0f));
+				triangles[triangleID] = {{v0.x, v0.y, v0.z}, {v1.x, v1.y, v1.z}, {v2.x, v2.y, v2.z}, {entity.ID, mesh.ID}};
 			}
 		}
-		indices.reserve(indicesCount);
-		for (size_t i = 0; i < trianglesSize; ++i)
-		{
-			if (triangles[i].userData.first == entity.ID && triangles[i].userData.second == mesh.ID)
-			{
-				indices.push_back(i);
-			}
-		}
-		auto indiceCount = mesh.indiceCount ? mesh.indiceCount(entity) : 0;
-		if (!indiceCount)
-			return;
-		auto meshIndices = mesh.indices(entity);
-		auto meshVertices = mesh.vertices(entity);
-		auto indicesData = meshIndices.data();
-		auto verticesData = meshVertices.data();
-		auto& model = entity.getModelMatrix();
-		for (size_t i = 0, c = 0; i < indiceCount; c++, i += 3)
-		{
-			auto& triangleID = indices[c];
-			auto i0 = indicesData[i + 0];
-			auto i1 = indicesData[i + 1];
-			auto i2 = indicesData[i + 2];
-			auto v0 = verticesData[i0];
-			auto v1 = verticesData[i1];
-			auto v2 = verticesData[i2];
-			v0 = glm::vec3(model * glm::vec4(v0, 1.0f));
-			v1 = glm::vec3(model * glm::vec4(v1, 1.0f));
-			v2 = glm::vec3(model * glm::vec4(v2, 1.0f));
-			triangles[triangleID] = {{v0.x, v0.y, v0.z}, {v1.x, v1.y, v1.z}, {v2.x, v2.y, v2.z}, {entity.ID, mesh.ID}};
-		}
+	}
+	for (auto& child : entity.children)
+	{
+		updateEntity(child);
 	}
 	built = false;
 	changed = true;
