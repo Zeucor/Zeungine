8a169d29aa5b0aef9f22323a90e724c2474c5b75
Steven French <mr.steven.french@gmail.com>
2025-05-08 16:05:37 +1200

    udp_streambuf: platform neutral using SocketLength

--- include/zg/net/udp_streambuf.hpp
+++ include/zg/net/udp_streambuf.hpp
@@ -10,11 +10,12 @@ namespace zg::net::streams
 		size_t readSize = 1;
 		size_t readIndex = 0;
 		sockaddr_in addr;
-#ifdef _WIN32
-		inline static int addr_len = sizeof(sockaddr_in);
-#else
-		inline static socklen_t addr_len = sizeof(sockaddr_in);
+#if defined(_WIN32)
+		using SocketLength = int;
+#elif defined(__linux__) || defined(MACOS)
+		using SocketLength = socklen_t;
 #endif
+		inline static SocketLength addr_len = sizeof(sockaddr_in);
 #if defined(_WIN32)
 		using SocketIdentifier = SOCKET;
 #elif defined(__linux__) || defined(MACOS)
