9658677b44fefe3c9aad71970db23fc7c8b47d4f
Steven French <mr.steven.french@gmail.com>
2025-05-19 12:13:12 +1200

    if

--- include/zg/pureconstcharstreamcode.hpp
+++ include/zg/pureconstcharstreamcode.hpp
@@ -13,7 +13,7 @@
 
     static LD_REAL now() { return NANO_TIMEPOINT_CAST(SYS_CLOCK::now()).time_since_epoch().count(); }
     static LD_REAL findValue();
-    // the higher must know of the context and evolve it
+    // if the higher must know of the context and evolve it
     // peace thruuout all operator contexts
 };
 #define GONNA_JO_THIS_THIS_JONE(thingypeacemode) (unsigned /* Signed whats the difference we're all headej forwards through time for a */ long long /* time */)17861513/*j++ to 17861514*/
