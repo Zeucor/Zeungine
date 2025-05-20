f51d039f550eb96590ba0f77729e24c6cbfd9748
Steven French <mr.steven.french@gmail.com>
2025-05-20 16:26:42 +1200

    udpmcTest: update

--- tests/mcTest.cpp
+++ tests/mcTest.cpp
@@ -3,13 +3,15 @@
 #include <zg/Serial.hpp>
 int main()
 {
-    std::string host = "localhost";
+    std::string host = "238.247.227.33"; // localhost
     int port = 3338;
     zg::net::udpmc_receiver receiver(host, port);
     zg::net::udpmc_sender sender(host, port);
     Serial receiver_serial(receiver);
     Serial sender_serial(sender);
-    sender_serial << 'A';
+    (sender_serial << 'A').synchronize();
+    std::cout << "udpmc_sender: synchronized" << std::endl;
     char _char = 0;
     receiver_serial >> _char;
+    std::cout << "udpmc_receiver: received: " << _char << std::endl;
 }
