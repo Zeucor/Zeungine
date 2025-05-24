0748a51e508470429b34fd6021001ece2b01cffc
Steven French <mr.steven.french@gmail.com>
2025-05-24 18:53:40 +1200

    EditorFactory: add key animations to panels

--- src/components/windows/Editor.cpp
+++ src/components/windows/Editor.cpp
@@ -3,6 +3,7 @@
 #include <zg/Window.hpp>
 #include <zg/entities/ui/Layout.hpp>
 #include <zg/entities/Plane.hpp>
+#include <zg/TimingFunction.hpp>
 using namespace zg::components::windows;
 using namespace zg;
 SceneCreateInfo EditorSceneFactory()
@@ -29,15 +30,25 @@ SceneCreateInfo EditorSceneFactory()
             glm::vec3 plane_2_size(0.5, 0.5, 1);
             auto plane_2_position = std::any_cast<glm::vec3>(vLayout_1.template setData<glm::vec3>("GetSubPosition", plane_2_size));
             auto plane_2_to_be_panel = entities::PlaneFactory(glm::vec4(0, 0, 1, 1), "Blue Plane", plane_2_position, rotate_identity, plane_2_size);
-            vLayout_1.addChild(plane_2_to_be_panel);
+            auto plane_2_tuple = vLayout_1.addChild(plane_2_to_be_panel);
+            auto plane_2_ID = std::get<KEY_ID_VECTOR_ID_INDEX>(plane_2_tuple);
+            auto& plane_2 = *std::get<KEY_ID_VECTOR_VALUE_INDEX>(plane_2_tuple);
+            plane_2.template make<bool>("Showing", true);
+            plane_2.template make<float>("AnimationT", 0.f);
+            plane_2.template make<std::shared_ptr<bool>>("AnimationRunning", std::shared_ptr<bool>{});
             glm::vec3 plane_3_size(0.0, 1.0, 1);
             auto plane_3_position = std::any_cast<glm::vec3>(vLayout_1.template setData<glm::vec3>("GetSubPosition", plane_3_size));
             auto plane_3_to_be_panel = entities::PlaneFactory(glm::vec4(1, 1, 1, 1), "No Size X Plane", plane_3_position, rotate_identity, plane_3_size);
             vLayout_1.addChild(plane_3_to_be_panel);
             glm::vec3 plane_4_size(1.5, 0.5, 1);
             auto plane_4_position = std::any_cast<glm::vec3>(vLayout_1.template setData<glm::vec3>("GetSubPosition", plane_4_size));
-            auto plane_4_to_be_panel = entities::PlaneFactory(glm::vec4(1, 1, 1, 1), "Greeen Plane", plane_4_position, rotate_identity, plane_4_size);
-            vLayout_1.addChild(plane_4_to_be_panel);
+            auto plane_4_to_be_panel = entities::PlaneFactory(glm::vec4(0, 1, 0, 1), "Greeen Plane", plane_4_position, rotate_identity, plane_4_size);
+            auto plane_4_tuple = vLayout_1.addChild(plane_4_to_be_panel);
+            auto plane_4_ID = std::get<KEY_ID_VECTOR_ID_INDEX>(plane_4_tuple);
+            auto& plane_4 = *std::get<KEY_ID_VECTOR_VALUE_INDEX>(plane_4_tuple);
+            plane_4.template make<bool>("Showing", true);
+            plane_4.template make<float>("AnimationT", 0.f);
+            plane_4.template make<std::shared_ptr<bool>>("AnimationRunning", std::shared_ptr<bool>{});
             auto vLayout_2_size = glm::vec3(0.5, 2, 1);
             auto vLayout_2_position = std::any_cast<glm::vec3>(hLayout.template setData<glm::vec3>("GetSubPosition", vLayout_2_size));
             auto vLayout_2_info = entities::ui::LayoutFactory("VLayout_2", entities::ui::LayoutDimension::Vertical, vLayout_2_position, vLayout_2_size, window.iRenderer, true);
@@ -46,13 +57,135 @@ SceneCreateInfo EditorSceneFactory()
             glm::vec3 plane_1_size(0.5, 2, 1);
             auto plane_1_position = std::any_cast<glm::vec3>(vLayout_2.template setData<glm::vec3>("GetSubPosition", plane_1_size));
             auto plane_1_to_be_panel = entities::PlaneFactory(glm::vec4(1, 0, 0, 1), "Red Plane", plane_1_position, rotate_identity, plane_1_size);
-            vLayout_2.addChild(plane_1_to_be_panel);
+            auto plane_1_tuple = vLayout_2.addChild(plane_1_to_be_panel);
+            auto plane_1_ID = std::get<KEY_ID_VECTOR_ID_INDEX>(plane_1_tuple);
+            auto& plane_1 = *std::get<KEY_ID_VECTOR_VALUE_INDEX>(plane_1_tuple);
+            plane_1.template make<bool>("Showing", true);
+            plane_1.template make<float>("AnimationT", 0.f);
+            plane_1.template make<std::shared_ptr<bool>>("AnimationRunning", std::shared_ptr<bool>{});
             // Setup HView / VView
             //  Add Panels
             //   Iterate Properties
+            auto& keyBools = scene.template make<std::shared_ptr<std::array<bool, 5>>>("KeyBools", std::make_shared<std::array<bool, 5>>());
+            scene.template make<size_t>("KeyPressID", window.addAnyKeyPressHandler([
+                SCENE_INDEX_STACK = scene.INDEX_STACK,
+                plane_1_ID,
+                start_1_pos = plane_1_position,
+                plane_2_ID,
+                start_2_pos = plane_2_position,
+                plane_4_ID,
+                start_4_pos = plane_4_position,
+                keyBools
+            ](const auto& key, auto pressed) {
+                auto key_index = -1;
+                switch (key)
+                {
+                case KEYCODE_CTRL:
+                    key_index = 0;
+                    break;
+                case KEYCODE_SHIFT:
+                    key_index = 1;
+                    break;
+                case '1':
+                    key_index = 2;
+                    break;
+                case '2':
+                    key_index = 3;
+                    break;
+                case '3':
+                    key_index = 4;
+                    break;
+                }
+                if (key_index == -1)
+                {
+                    return;
+                }
+                auto& key_bools = *keyBools;
+                key_bools[key_index] = pressed;
+                std::cout << "Key_index: " << key_index << ", pressed: " << pressed << std::endl;
+                if (!pressed)
+                {
+                    return;
+                }
+                auto& rgy = Registry::GetSingleton();
+                auto& window = rgy.getWindow(SCENE_INDEX_STACK);
+                bool ctrlShiftPressed = key_bools[0];
+                if (!ctrlShiftPressed)
+                {
+                    return;
+                }
+                auto& scene = rgy.getScene(SCENE_INDEX_STACK);
+                switch (key)
+                {
+                case '1':
+                {
+                    auto& plane_2 = rgy.getEntity(plane_2_ID);
+                    auto& showing = plane_2.template getData<bool>("Showing");
+                    showing = !showing;
+                    auto& animationT = plane_2.template getData<float>("AnimationT");
+                    auto& animationRunning = plane_2.template getData<std::shared_ptr<bool>>("AnimationRunning");
+                    if (animationRunning && *animationRunning)
+                    {
+                        *animationRunning = false;
+                    }
+                    animationT = 0.f;
+                    glm::vec3 end_2_pos = start_2_pos + glm::vec3(0.0, 0.5, 0);
+                    glm::vec3 _start_2_pos = plane_2.position;
+                    if (showing)
+                    {
+                        end_2_pos = start_2_pos;
+                    }
+                    animationRunning = StartTimingFunction(window, _start_2_pos, end_2_pos, &plane_2.position, animationT, 1, Easing::easeInCubic);
+                    break;
+                };
+                case '2':
+                {
+                    auto& plane_4 = rgy.getEntity(plane_4_ID);
+                    auto& showing = plane_4.template getData<bool>("Showing");
+                    showing = !showing;
+                    auto& animationT = plane_4.template getData<float>("AnimationT");
+                    auto& animationRunning = plane_4.template getData<std::shared_ptr<bool>>("AnimationRunning");
+                    if (animationRunning && *animationRunning)
+                    {
+                        *animationRunning = false;
+                    }
+                    animationT = 0.f;
+                    glm::vec3 end_4_pos = start_4_pos + glm::vec3(0.0, -0.5, 0);
+                    glm::vec3 _start_4_pos = plane_4.position;
+                    if (showing)
+                    {
+                        end_4_pos = start_4_pos;
+                    }
+                    animationRunning = StartTimingFunction(window, _start_4_pos, end_4_pos, &plane_4.position, animationT, 1, Easing::easeInCubic);
+                    break;
+                };
+                case '3':
+                {
+                    auto& plane_1 = rgy.getEntity(plane_1_ID);
+                    auto& showing = plane_1.template getData<bool>("Showing");
+                    showing = !showing;
+                    auto& animationT = plane_1.template getData<float>("AnimationT");
+                    auto& animationRunning = plane_1.template getData<std::shared_ptr<bool>>("AnimationRunning");
+                    if (animationRunning && *animationRunning)
+                    {
+                        *animationRunning = false;
+                    }
+                    animationT = 0.f;
+                    glm::vec3 end_1_pos = start_1_pos + glm::vec3(0.5, 0.0, 0);
+                    glm::vec3 _start_1_pos = plane_1.position;
+                    if (showing)
+                    {
+                        end_1_pos = start_1_pos;
+                    }
+                    animationRunning = StartTimingFunction(window, _start_1_pos, end_1_pos, &plane_1.position, animationT, 1, Easing::easeInCubic);
+                    break;
+                };
+                }
+            }));
         },
         .onDetachedFunction = [](auto& scene) {
-
+            auto& window = Registry::GetSingleton().getWindow(scene.INDEX_STACK);
+            window.removeAnyKeyPressHandler(scene.template getData<size_t>("KeyPressID"));
         }
     };
     return info;
