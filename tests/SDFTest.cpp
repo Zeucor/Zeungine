77f92a9774a3d7c79e962544f794655bbb266da9
Steven French <mr.steven.french@gmail.com>
2025-05-26 16:57:44 +1200

    SDFTest: increate framerate to 250

--- tests/SDFTest.cpp
+++ tests/SDFTest.cpp
@@ -31,7 +31,7 @@ int main()
         .windowY = 0,
         .borderless = true,
         .vsync = false,
-        .framerate = 200
+        .framerate = 250
     };
     auto window_tuple = Registry::GetSingleton().addWindow(windowInfo);
     auto& window = *std::get<KEY_ID_VECTOR_VALUE_INDEX>(window_tuple);
