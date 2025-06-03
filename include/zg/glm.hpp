7c94411717218c151e5745fd6d1f4c7c9a25bdd2
Steven French <mr.steven.french@gmail.com>
2025-06-03 15:25:30 +1200

    zg impls of min/max

--- include/zg/glm.hpp
+++ include/zg/glm.hpp
@@ -18,3 +18,30 @@ inline static float ZG_PI = acos(-1);
 inline static float ZG_PI_2 = ZG_PI * 2.f;
 using uvec = glm::vec<4, uint8_t>;
 #define rotate_identity {1, 0, 0, 0}
+namespace zg
+{
+	// Fast max and min assuming no side effects and no NaNs
+	// Inline and constexpr for compile-time and runtime optimization
+	template <typename T>
+	inline constexpr T max(const T& a, const T& b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	template <typename T>
+	inline constexpr T min(const T& a, const T& b)
+	{
+		return (a < b) ? a : b;
+	}
+
+	// Vectorized versions for glm::vec3 (element-wise)
+	inline glm::vec3 max(const glm::vec3& a, const glm::vec3& b)
+	{
+		return glm::vec3(max(a.x, b.x), max(a.y, b.y), max(a.z, b.z));
+	}
+
+	inline glm::vec3 min(const glm::vec3& a, const glm::vec3& b)
+	{
+		return glm::vec3(min(a.x, b.x), min(a.y, b.y), min(a.z, b.z));
+	}
+}
